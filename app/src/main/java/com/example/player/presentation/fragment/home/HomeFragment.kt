d55eb32a5fc227770ce24f25e12692ff8b7402a7
WAR10RD <axal3241@gmail.com>
2022-05-13 13:18:32 +0530

    -[Added] BottomNavigation bar

--- app/src/main/java/com/example/player/presentation/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/player/presentation/fragment/home/HomeFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.player.R
 import com.example.player.databinding.FragmentHomeBinding
+import com.example.player.presentation.fragment.all_folder.AllFolderFragment
+import com.example.player.presentation.fragment.all_videos.AllVideosFragment
 
 class HomeFragment : Fragment() {
 
@@ -30,7 +33,33 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /** Setting first fragment */
+        setFragment(AllVideosFragment())
 
+        /** SetListener */
+        setListener()
+
+    }
+
+    /** SetListener */
+    private fun setListener() {
+
+        /** Bottom navigation bar item click listener */
+        binding.bnb.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.allVideos -> setFragment(AllVideosFragment())
+                R.id.allFolder -> setFragment(AllFolderFragment())
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    /**
+     * Setting fragment in frameLayout
+     * @param fragment
+     */
+    private fun setFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction().replace(R.id.fl, fragment).commit()
     }
 
 }
